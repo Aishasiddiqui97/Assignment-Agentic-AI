3627d2916b796d94a7aaa6f66ad6607a80cb6e98
AishaSiddiqui <aishaanjumsiddiqui97@gmail.com>
2025-06-17 23:47:10 +0500

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,28 @@
+#RUN
+import asyncio
+from openai import AsyncOpenAI
+from agents import Agent, OpenAIChatCompletionsModel, Runner, set_tracing_disabled
+
+gemini_api_key = "AIzaSyDhD_WyAAS7eXi44gz1dvaWmqYlL9U_ah0"
+
+client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+set_tracing_disabled(disabled=True)
+
+async def main():
+    print("Running...")  # Debug print
+    agent = Agent(
+        name="Raju",
+        instructions="The agent makes decisions based on context, like user location, mood, or recent actions.",
+        model=OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=client),
+    )
+
+    result = await Runner.run(
+        agent,
+        "You think I’m stupid? I see everything — I just don’t say anything!" )
+    print(result.final_output)
+
+asyncio.run(main())
